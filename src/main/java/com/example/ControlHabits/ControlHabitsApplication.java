454c870cba595d7f0b14d38c6ac7fd2ce55eb2b9
icaroivcd <icaro.v84@gmail.com>
2026-03-12 19:11:40 -0300

    first commit

---
+++ src/main/java/com/example/ControlHabits/ControlHabitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ControlHabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlHabitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlHabitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ControlHabits/ControlHabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ControlHabits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlHabitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
