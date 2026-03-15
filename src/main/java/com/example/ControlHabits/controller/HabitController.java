132a7a35ff82f4318418ffd69354f83155a6f5aa
icaroivcd <icaro.v84@gmail.com>
2026-03-15 20:23:10 -0300

    fix: refatora create e update para uso de DTO

--- src/main/java/com/example/ControlHabits/controller/HabitController.java
+++ src/main/java/com/example/ControlHabits/controller/HabitController.java
@@ -1,7 +1,10 @@
 package com.example.ControlHabits.controller;
 
+import com.example.ControlHabits.dto.CreateHabitDTO;
+import com.example.ControlHabits.dto.UpdateHabitDTO;
 import com.example.ControlHabits.entity.Habit;
 import com.example.ControlHabits.service.HabitService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +19,7 @@ public class HabitController {
     @Autowired
     private HabitService habitService;
 
-    @DeleteMapping("delet/{id}")
+    @DeleteMapping("delete/{id}")
     public ResponseEntity<Void> deleteHabit(@PathVariable Long id) {
         habitService.deleteHabit(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -28,19 +31,27 @@ public class HabitController {
     }
 
     @GetMapping("/{id}")
-    public Habit getHabitById(@PathVariable Long id) {
-        return habitService.getHabitById(id);
+    public ResponseEntity<Habit> getHabitById(@PathVariable Long id) {
+        Habit habitId = habitService.getHabitById(id);
+        if (habitId == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity<>(habitId, HttpStatus.OK);
     }
 
     @PostMapping("/create-habit")
-    public Habit createHabits(@RequestBody Habit habit) {
-        return habitService.createHabits(habit);
+    public ResponseEntity<Habit> createHabits(@Valid @RequestBody CreateHabitDTO habit) {
+        Habit created = habitService.createHabits(habit);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
     @PatchMapping("/update/{id}")
-    public ResponseEntity<Habit> updateHabit(@PathVariable Long id, @RequestBody Habit habit){
-        habitService.updateHabit(id, habit);
-        return new ResponseEntity<>(habit, HttpStatus.OK);
-    }
+    public ResponseEntity<Habit> updateHabit(@PathVariable Long id, @RequestBody UpdateHabitDTO habit) {
+        Habit updatedHabit = habitService.updateHabit(id, habit);
 
+        if(updatedHabit == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedHabit);
+    }
 }
